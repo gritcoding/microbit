10feb03534f898179a42d6b518c6c8ae0412f602
Naeem Ahmed <naeem.ahmed@gmail.com>
2017-11-02 05:50:48 +0800

    teach unlimited

---
+++ js/voting_send_with_rps.js
@@ -0,0 +1,38 @@
+let weapon = 0
+input.onButtonPressed(Button.A, () => {
+    radio.sendNumber(0)
+    basic.showString("Y")
+})
+input.onButtonPressed(Button.B, () => {
+    radio.sendNumber(1)
+    basic.showString("N")
+})
+input.onGesture(Gesture.Shake, () => {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(200)
+    weapon = Math.random(3)
+    if (weapon == 0) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else if (weapon == 1) {
+        basic.showIcon(IconNames.Square)
+    } else {
+        basic.showIcon(IconNames.Scissors)
+    }
+})
+radio.setGroup(64)
+radio.setTransmitSerialNumber(true)
+basic.forever(() => {
+	
+})
